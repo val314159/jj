dab231befb8b73d88bb1941b4d6ec2e8b1d8deaa
val314159 <jmward@gmail.com>
2015-05-26 21:09:15 -0700

    Update jj.py

--- jj.py
+++ jj.py
@@ -1,6 +1,11 @@
 import sys,jinja2,yamli
-def main( tmpl_text, data_text ):
-    data = yamli.load( data_text )
-    tmpl = jinja2.Template( tmpl_text ).
-    f_out.write( tmpl.render( ** data ) )
+def load_tmpl( tmpl_file=0, tmpl_dir=0 ):
+    loader = jinja2.FileSystemLoader(tmpl_dir or '.')
+    env = jinja2.Environment(loader=loader)
+    return env.get_template(tmpl_file or sys.stdin)
+def main( tmpl_name, data_name=None ):
+    data = yamli.load( open(data_name) ) \
+        if data_name else {}
+    print load_tmpl( tmpl_name ).render( **data )
+    pass
 if __name__=='__main__': main( *sys.argv )
